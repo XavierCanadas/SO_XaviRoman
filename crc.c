e7c98ca94f22319edbf49f5ea8ca5e7252ef93ab
XavierCanadas <xavier.canadas01@estudiant.upf.edu>
2024-02-27 18:44:37 +0100

    Lab 5

--- crc.c
+++ crc.c
@@ -14,7 +14,7 @@
  * notice must not be changed or removed and no warranty is either
  * expressed or implied by its publication or distribution.
  **********************************************************************/
-
+ 
 #include "crc.h"
 
 
@@ -29,7 +29,7 @@
 #define REFLECT_DATA(X)			((unsigned char) reflect((X), 8))
 #else
 #undef  REFLECT_DATA
-#define REFLECT_DATA(X)            (X)
+#define REFLECT_DATA(X)			(X)
 #endif
 
 #if (REFLECT_REMAINDER == TRUE)
@@ -37,7 +37,7 @@
 #define REFLECT_REMAINDER(X)	((crc) reflect((X), WIDTH))
 #else
 #undef  REFLECT_REMAINDER
-#define REFLECT_REMAINDER(X)    (X)
+#define REFLECT_REMAINDER(X)	(X)
 #endif
 
 
@@ -54,27 +54,30 @@
  *
  *********************************************************************/
 static unsigned long
-reflect(unsigned long data, unsigned char nBits) {
-    unsigned long reflection = 0x00000000;
-    unsigned char bit;
+reflect(unsigned long data, unsigned char nBits)
+{
+	unsigned long  reflection = 0x00000000;
+	unsigned char  bit;
 
-    /*
-     * Reflect the data about the center bit.
-     */
-    for (bit = 0; bit < nBits; ++bit) {
-        /*
-         * If the LSB bit is set, set the reflection of it.
-         */
-        if (data & 0x01) {
-            reflection |= (1 << ((nBits - 1) - bit));
-        }
+	/*
+	 * Reflect the data about the center bit.
+	 */
+	for (bit = 0; bit < nBits; ++bit)
+	{
+		/*
+		 * If the LSB bit is set, set the reflection of it.
+		 */
+		if (data & 0x01)
+		{
+			reflection |= (1 << ((nBits - 1) - bit));
+		}
 
-        data = (data >> 1);
-    }
+		data = (data >> 1);
+	}
 
-    return (reflection);
+	return (reflection);
 
-}    /* reflect() */
+}	/* reflect() */
 
 
 /*********************************************************************
@@ -88,16 +91,19 @@ reflect(unsigned long data, unsigned char nBits) {
  * Returns:		The CRC of the message.
  *
  *********************************************************************/
-crc crcSlow(unsigned char const message[], int nBytes) {
-    crc remainder = INITIAL_REMAINDER;
-    int byte;
-    unsigned char bit;
+crc
+crcSlow(unsigned char const message[], int nBytes)
+{
+    crc            remainder = INITIAL_REMAINDER;
+	int            byte;
+	unsigned char  bit;
 
 
     /*
      * Perform modulo-2 division, a byte at a time.
      */
-    for (byte = 0; byte < nBytes; ++byte) {
+    for (byte = 0; byte < nBytes; ++byte)
+    {
         /*
          * Bring the next byte into the remainder.
          */
@@ -106,13 +112,17 @@ crc crcSlow(unsigned char const message[], int nBytes) {
         /*
          * Perform modulo-2 division, a bit at a time.
          */
-        for (bit = 8; bit > 0; --bit) {
+        for (bit = 8; bit > 0; --bit)
+        {
             /*
              * Try to divide the current data bit.
              */
-            if (remainder & TOPBIT) {
+            if (remainder & TOPBIT)
+            {
                 remainder = (remainder << 1) ^ POLYNOMIAL;
-            } else {
+            }
+            else
+            {
                 remainder = (remainder << 1);
             }
         }
@@ -126,7 +136,7 @@ crc crcSlow(unsigned char const message[], int nBytes) {
 }   /* crcSlow() */
 
 
-crc crcTable[256];
+crc  crcTable[256];
 
 
 /*********************************************************************
@@ -143,16 +153,18 @@ crc crcTable[256];
  *
  *********************************************************************/
 void
-crcInit(void) {
-    crc remainder;
-    int dividend;
-    unsigned char bit;
+crcInit(void)
+{
+    crc			   remainder;
+	int			   dividend;
+	unsigned char  bit;
 
 
     /*
      * Compute the remainder of each possible dividend.
      */
-    for (dividend = 0; dividend < 256; ++dividend) {
+    for (dividend = 0; dividend < 256; ++dividend)
+    {
         /*
          * Start with the dividend followed by zeros.
          */
@@ -161,13 +173,17 @@ crcInit(void) {
         /*
          * Perform modulo-2 division, a bit at a time.
          */
-        for (bit = 8; bit > 0; --bit) {
+        for (bit = 8; bit > 0; --bit)
+        {
             /*
              * Try to divide the current data bit.
-             */
-            if (remainder & TOPBIT) {
+             */			
+            if (remainder & TOPBIT)
+            {
                 remainder = (remainder << 1) ^ POLYNOMIAL;
-            } else {
+            }
+            else
+            {
                 remainder = (remainder << 1);
             }
         }
@@ -193,18 +209,20 @@ crcInit(void) {
  *
  *********************************************************************/
 crc
-crcFast(unsigned char const message[], int nBytes) {
-    crc remainder = INITIAL_REMAINDER;
-    unsigned char data;
-    int byte;
+crcFast(unsigned char const message[], int nBytes)
+{
+    crc	           remainder = INITIAL_REMAINDER;
+    unsigned char  data;
+	int            byte;
 
 
     /*
      * Divide the message by the polynomial, a byte at a time.
      */
-    for (byte = 0; byte < nBytes; ++byte) {
+    for (byte = 0; byte < nBytes; ++byte)
+    {
         data = REFLECT_DATA(message[byte]) ^ (remainder >> (WIDTH - 8));
-        remainder = crcTable[data] ^ (remainder << 8);
+  		remainder = crcTable[data] ^ (remainder << 8);
     }
 
     /*
--- main.c
+++ main.c
@@ -1,114 +1,78 @@
-#include "crc.h"
-#include "fileManager.h"
-#include "myutils.h"
+#include <fcntl.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // for sleep function : waits for seconds
+#include <sys/stat.h>
+#include <time.h>   // for usleep waits for microsec
+#include <unistd.h> // for sleep: waits for seconds
+#include "crc.h"
+#include "fileLock.h"
+#define N 4
 
-FileManager fm;
-int errors = 0;
+typedef struct {
+    int nBlock;
+    int isGet;
+} Request;
+
+typedef struct {
+    int nBlock;
+    short int crc;
+} Result;
 
 
-void *worker_function(void *arg) {
-    int nBytesReadData = 1;
+int main(int argc, char * argv[]) {
+    int pipeA[2], pipeB[2];
+    // Create two pipes. Also remember to close the channels when needed, otherwise it will not work!
 
-    while (fm.nFilesRemaining > 0) {
-        dataEntry d;
-        char buff[256];
-        crc codiComputatCRC;
+    for (int i = 0; i < N; ++i) {
+        int n = fork();
+        if (n == 0) {
+            int fd = open(argv[1], O_RDONLY);
+            int fdCRC =open(argv[2], O_RDWR);
+            Request r;
+            while( /* Read a request from the pipe */) {
+                if (!r.isGet) {
 
-        // Passa un thread perquè reservi un fitxer.
-        my_sem_wait(&semafor);
+                    /* Recompute the CRC, use lseek to get the correct datablock,
+                    and store it in the correct position of the CRC file. Remember to use approppriate locks! */
+                    usleep(rand()%1000 *1000); // Make the computation a bit slower
 
-        // El thread q ha passat executa la funció per agafar un fitxer q llegirà. Els locks es fan dintre de la funció.
-        int res = getAndReserveFile(&fm, &d);
+                }
+                else{
+                    usleep(rand()%1000 *1000);
+                    Result res;
+                    res.nBlock = r.nBlock;
+                    // Read the CRC from the CRC file, using lseek + read. Remember to use the correct locks!
 
-        if (res == 0) {
-            // es llegeix el fitxer
-            read(d.fdcrc, &codiComputatCRC, sizeof(crc));
-            nBytesReadData = read(d.fddata, buff, 255);
-
-            crc crcLlegit = crcSlow((unsigned char* const) buff, nBytesReadData);
-
-            if (codiComputatCRC != crcLlegit ) {
-                errors++;
-                printf("\nCRC error in file %d\n", d.fddata);
-                printf("el crc ha donat %d i el fitxer hi havia %d\n", codiComputatCRC, crcLlegit);
+                    //Write the result in pipeB!
+                }
             }
 
-            // Després de llegir i fer les operacions del crc, es crida a unreserveFile posar-la disponible per llegir un altre bloc.
-            unreserveFile(&fm, &d);
-
-            // Si s'ha arribat al final, es marca com acabada.
-            if (nBytesReadData <= 0) {
-                markFileAsFinished(&fm, &d);
-            }
+            exit(0);
         }
     }
-    my_sem_signal(&semafor);
+    char s[100];
+    int nBytesRead;
+    /* Read until the standard output*/
+    while((nBytesRead = read(0, s, 100) ) > 0) {
+        char op[200];
+        s[nBytesRead] = '\0';
+        int nBlock;
 
-    return NULL;
-}
-
-int main(int argc, char **argv) {
-
-
-    initialiseFdProvider(&fm, argc, argv);
-
-    pthread_mutex_init(&lock, NULL);
-
-
-
-    // S'inicialitza el semàfor. L'espai q es dona és del nombre de fitxers que s'ha passat.
-    my_sem_init(&semafor, 1);
-
-
-    int N = 10;
-
-
-    pthread_t threadID[N];
-    for (int i = 0; i < N; ++i) {
-        pthread_t thread;
-        pthread_create(&threadID[i], NULL, worker_function, NULL);
+        sscanf(s, "%s %d", op, &nBlock);
+        Request r;
+        r.nBlock = nBlock;
+        r.isGet = strcmp(op, "get") == 0;
+        // Write r in the pipe!
     }
 
+    printf("FINISHED\n");
+    while(wait(NULL) == -1);
 
-    for (int i = 0; i < N; ++i) {
-        pthread_join(threadID[i], NULL);
+    // Now that is finished, write all the results
+    Result res;
+    while((nBytesRead = read(pipeB[0], &res, sizeof(res)) ) > 0) {
+        printf("The CRC of block #%d is %d \n", res.nBlock, res.crc);
     }
-
-    printf("\nHi ha hagut %d errors\n\n", errors);
-
-    pthread_mutex_destroy(&lock);
-
-    destroyFdProvider(&fm);
-
-
-    /* Comprovació del fitxer sense threads
-    int file, fileCRC, errors = 0;
-
-    char path[100];
-    strcpy(path, argv[1]);
-    strcat(path, ".crc");
-    file = open(argv[1], O_RDONLY);
-    fileCRC = open(path, O_RDONLY);
-
-    char buffer[256];
-    crc crcComputat, crcLlegit;
-
-    while (read(file, buffer, 255) > 0) {
-        crcComputat = crcSlow((unsigned char* const) buffer, 255);
-
-        read(fileCRC, &crcLlegit, sizeof(crc));
-
-        if (crcLlegit != crcComputat)
-            errors++;
-    }
-    printf("Hi ha hagut %d errors\n", errors);
-     */
-
-
-    exit(0);
 }
