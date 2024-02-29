e7445d2e6c662054cde0bd7e6433528e7abf0749
XavierCanadas <xavier.canadas01@estudiant.upf.edu>
2024-02-29 21:41:23 +0100

    acabat

--- fileManager.c
+++ fileManager.c
@@ -10,8 +10,6 @@ my_semaphore semafor;
  */
 void initialiseFdProvider(FileManager *fm, int argc, char **argv) {
 
-
-
     // Complete the initialisation
     /* Your rest of the initailisation comes here*/
     fm->nFilesTotal = argc - 1;
@@ -43,17 +41,6 @@ void initialiseFdProvider(FileManager *fm, int argc, char **argv) {
         if(fm->fdCRC[i] < 0){
             printf("could not open crc file");
         }
-
-        /*
-         * una cosa podrías poner este print aquí y después otro en markFileFinishes.
-         * Quiero ver en que orden se procesan tus .txt
-         * esto lo q hace es imprimir el codigo del archivo
-         */
-
-        printf("fitxer %s, amb codi %d\n", argv[i + 1], fm->fdData[i]);
-        //printf("fitxer crc %s, amb codi %d\n", path, fm->fdCRC[i]);
-
-
     }
 }
 
@@ -75,16 +62,9 @@ void destroyFdProvider(FileManager *fm) {
 // S'ha afegit un lock
 int getAndReserveFile(FileManager *fm, dataEntry *d) {
 
-    // Fer lock perquè el thread comprovi si hi ha algun fitxer disponible.
-    //pthread_mutex_lock(&lock);
-
-    // This function needs to be implemented by the students
-    //int i;
-
     for (int i = 0; i < fm->nFilesTotal; ++i) {
         my_sem_wait(&semafor);
         if (fm->fileAvailable[i] == 1 && fm->fileFinished[i] == 0) {
-            //pthread_mutex_lock(&lock);
             d->fdcrc = fm->fdCRC[i];
             d->fddata = fm->fdData[i];
             d->index = i;
@@ -94,10 +74,8 @@ int getAndReserveFile(FileManager *fm, dataEntry *d) {
 
             // You should mark that the file is not available
             fm->fileAvailable[i] = 0;
-            //printf("Fitxer %d agafat, el available %d\n", fm->fdData[i], fm->fileAvailable[i]);
 
-            // Una vegada s'ha trobat el fitxer i assignat al dataEntry, es fa un unlock.
-            //pthread_mutex_unlock(&lock);
+            // Una vegada s'ha trobat el fitxer i assignat al dataEntry, es fa un signal.
             my_sem_signal(&semafor);
 
 
@@ -105,38 +83,23 @@ int getAndReserveFile(FileManager *fm, dataEntry *d) {
         }
         my_sem_signal(&semafor);
     }
-
-    // Si no s'ha trobat cap fitxer es fa unlock.
-    //pthread_mutex_unlock(&lock);
-
-
     return 1;
 }
 
 void unreserveFile(FileManager *fm, dataEntry *d) {
-    // es fa lock perquè es tocarà memòria compartida
-
     fm->fileAvailable[d->index] = 1;
     endTimer(d->index);
 
-    //my_sem_signal(&semafor);
 }
 
 void markFileAsFinished(FileManager *fm, dataEntry *d) {
     // es fa lock perquè es tocarà memòria compartida
-    //pthread_mutex_lock(&lock);
     my_sem_wait(&semafor);
-    printf("S'ha tancat el fitxer amb codi %d\n", fm->fdData[d->index]);
 
     fm->fileFinished[d->index] = 1;
     fm->nFilesRemaining--; //mark that a file has finished
     my_sem_signal(&semafor);
     if (fm->nFilesRemaining == 0) {
         printf("\nAll files have been processed\n");
-        //TO COMPLETE: unblock all waiting threads, if needed
-        //my_sem_signal(&semafor);
-        // el signal el fem al unreverseFile i aquest sempre s'executa abans de cridar a aquesta funció
     }
-
-   // pthread_mutex_unlock(&lock);
 }
--- main.c
+++ main.c
@@ -29,24 +29,20 @@ void *worker_function(void *arg) {
         // El thread q ha passat executa la funció per agafar un fitxer q llegirà. Els locks es fan dintre de la funció.
         int res = getAndReserveFile(&fm, &d);
 
-       // my_sem_signal(&semafor);
-       // pthread_mutex_unlock(&lock);
-
-
         if (res == 0) {
             // es llegeix el fitxer
             read(d.fdcrc, &codiComputatCRC, sizeof(crc));
             int nBytesReadData = read(d.fddata, buff, 255);
 	        //printf("%d - %s\n", nBytesReadData, buff );
             crc crcLlegit = crcSlow((unsigned char* const) buff, nBytesReadData);
-            //pthread_mutex_unlock(&lock);
+
+            // es fa lock perquè es toca una variable comuna;
+            pthread_mutex_lock(&lock);
             if (fm.fileFinished[d.index] == 0 && codiComputatCRC != crcLlegit) {
                 errors++;
-                printf("\nCRC error in file %d\n", d.fddata);
-                printf("el crc ha donat %d i el fitxer hi havia %d\n", codiComputatCRC, crcLlegit);
-                printf("El fitxer està disponible: %d acabat: %d\n", fm.fileAvailable[d.index], fm.fileFinished[d.index]);
             }
-            //pthread_mutex_unlock(&lock);
+
+            pthread_mutex_unlock(&lock);
 
 
             // Després de llegir i fer les operacions del crc, es crida a unreserveFile posar-la disponible per llegir un altre bloc.
@@ -62,8 +58,6 @@ void *worker_function(void *arg) {
         }
 
     }
-    //my_sem_signal(&semafor);
-
     return NULL;
 }
 
@@ -73,9 +67,6 @@ int main(int argc, char **argv) {
     initialiseFdProvider(&fm, argc, argv);
 
     pthread_mutex_init(&lock, NULL);
-
-    // S'inicialitza el semàfor. L'espai q es dona és del nombre de fitxers que s'ha passat.
-
 
     int N = 4;
     pthread_t threadID[N];
@@ -89,36 +80,16 @@ int main(int argc, char **argv) {
         pthread_join(threadID[i], NULL);
     }
 
-    printf("\nHi ha hagut %d errors\n", errors);
+    printf("\n**********************************************************************************************\n");
+    printf("\nS'han processat %d fitxers i hi ha hagut %d errors!\n", argc - 1, errors);
 
-    //pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&lock);
 
     destroyFdProvider(&fm);
 
+    printf("El temps que s'ha trigat en verificar els fitxers és: %ld\n\n", endTimer(0));
+    printf("\n**********************************************************************************************\n");
 
-    /* Comprovació del fitxer sense threads
-    int file, fileCRC, errors = 0;
 
-    char path[100];
-    strcpy(path, argv[1]);
-    strcat(path, ".crc");
-    file = open(argv[1], O_RDONLY);
-    fileCRC = open(path, O_RDONLY);
-
-    char buffer[256];
-    crc crcComputat, crcLlegit;
-
-    while (read(file, buffer, 255) > 0) {
-        crcComputat = crcSlow((unsigned char* const) buffer, 255);
-
-        read(fileCRC, &crcLlegit, sizeof(crc));
-
-        if (crcLlegit != crcComputat)
-            errors++;
-    }
-    printf("Hi ha hagut %d errors\n", errors);
-     */
-
-    printf("time is %ld\n\n", endTimer(0));
     exit(0);
 }
